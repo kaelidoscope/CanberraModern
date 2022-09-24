48b1667986bc79520eec1c4d7695c267a02d0819
Kaelidoscope <62172557+kaelidoscope@users.noreply.github.com>
2022-09-24 21:15:15 +1000

    More Pages Added
    Attempt to edit building codes as templates for other pages

---
+++ cbr-modern-master/components/EventsList.vue
@@ -0,0 +1,41 @@
+<!-- this component returns a list of all the events (Hopefully)-->
+<template>
+	<div class="">
+		<!-- these would show while the data loads or if an error -->
+		<p v-if="$fetchState.pending">Loading events...</p>
+		<p v-else-if="$fetchState.error">Error while fetching events</p>
+
+		<ul v-else class="list-group">
+			<!-- this is a for loop, it just loops through the list of events returned from the API -->
+			<li v-for="event in events" :key="event.id" class="list-group-item">
+				<!-- now make a link for each item -->
+				<!-- <NuxtLink :to="event.slug"> -->
+				<NuxtLink :to="'/events/' + events.slug">
+					<!-- return the rendered title -->
+					{{  events.title.rendered  }}
+				</NuxtLink>
+				<!-- now show me the event year -->
+				: {{ event.acf.year }}
+			</li>
+		</ul>
+		<h3>this list is stored in components/EventsList.vue</h3>
+
+	</div>
+</template>
+
+
+<script>
+export default {
+	// layout: 'home',
+	async fetch() {
+		this.events = await fetch('http://cm.beneb.com/wp-json/wp/v2/events/?per_page=20').then((res) =>
+			res.json()
+		)
+	},
+	data() {
+		return {
+			events: [],
+		}
+	},
+}
+</script>
---
+++ cbr-modern-master/pages/about/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="container">
+		<h2>About Canberra Modern</h2>
+		<h3>Filename: pages/buildings/index.vue</h3>
+
+		<SiteNavigation />
+
+
+		<div class="about">
+			<!-- The building list is over in the components folder -->
+			<!-- the code below loads the component here so we can use it again -->
+			<AboutPage />
+
+			<!-- this is a really useful line of code that will just output what the API call returns -->
+			<!-- <pre>{{ $data }}</pre> -->
+		</div>
+	</div>
+</template>
+
+<script>
+// in this API call, we go get a specific building, filtered by the ID in the URL
+// we access what is in the URL by using the params object
+export default {
+	async asyncData({ params }) {
+		const building = await fetch(
+			`http://cm.beneb.com/about/`
+		).then((res) => {
+			if (res.ok) {
+				return res.json()
+			}
+			throw new Error(res.status)
+		})
+		return { building }
+	},
+}
+</script>
+
+
+<style>
+</style>
