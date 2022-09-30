c9c0157ae437f0e42bf63738c0e1fdf6ef121596
Kaelidoscope <62172557+kaelidoscope@users.noreply.github.com>
2022-09-30 15:33:52 +1000

    OMZ WIORKING llinks
    Bare bone skeleton site

--- cbr-modern-master/components/SiteNavigation.vue
+++ cbr-modern-master/components/SiteNavigation.vue
@@ -1,17 +1,17 @@
 <!-- this component can hold site navigation -->
 <!-- if we put it here we can avoid having to repeat the code -->
 <template>
-	<nav>
-  <NuxtLink to="/">
+  <nav>
+    <NuxtLink to="/">
       Home
     </NuxtLink>
     <NuxtLink to="/about">
       About
     </NuxtLink>
-    <NuxtLink to="/events2">
+    <NuxtLink to="/events">
       Events
     </NuxtLink>
-		<NuxtLink to="/buildings">
+    <NuxtLink to="/buildings">
       Buildings
     </NuxtLink>
     <NuxtLink to="/videos">
@@ -23,10 +23,10 @@
     <NuxtLink to="/partners">
       Partners
     </NuxtLink>
-	</nav>
+  </nav>
 </template>
 
 <script>
-import NuxtLogo from './NuxtLogo.vue';
+//import NuxtLogo from './NuxtLogo.vue';
 
 </script>
---
+++ cbr-modern-master/pages/videos.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="container">
+		<h2>Home</h2>
+		<h3>Filename: pages/videos.vue</h3>
+		<SiteNavigation />
+
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'IndexPage'
+}
+</script>
